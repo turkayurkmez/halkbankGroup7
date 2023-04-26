190597292d6de934eac64d78701e50e3ba24d6a8
Türkay Ürkmez <turkay@turkayurkmez.com>
2023-04-26 16:06:39 +0300

    Abstract and interface completed

---
+++ Csharp/OOP/OOPOverview/AbstractANDInterface/Document.cs
@@ -0,0 +1,16 @@
+﻿namespace AbstractANDInterface
+{
+    /*
+     * Doküman Yönetim Sistemi Geliştiriyoruz.....
+     * Excel, Word, PDF, PowerPoint
+     */
+    public class Document
+    {
+        public string Title { get; set; }
+        public string Owner { get; set; }
+        public void Copy(string from, string to)
+        {
+            Console.WriteLine($"Doküman, {from} adresinden {to} adresine kopyalandı");
+        }
+    }
+}
